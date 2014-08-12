c67257e0b7943548e36acdfa6604f03ff3690940
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2014-08-12 14:30:07 +0300

    Old commits

---
+++ Zproject/00_suoz_project.bat
@@ -0,0 +1,1 @@
+call zig4.exe -r 42_abf_wgt_cnd_cmt_int_hms/42_abf_wgt_cnd_cmt_int_hms.dat 42_abf_wgt_cnd_cmt_int_hms/42_abf_wgt_cnd_cmt_int_hms.spp output/42_abf_wgt_cnd_cmt_int_hms/result_42_abf_wgt_cnd_cmt_int_hms.txt 0 0 1 0 --grid-output-formats=compressed-img
